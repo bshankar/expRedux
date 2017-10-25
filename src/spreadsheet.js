de5712fe9dccc3a064f742ecf48f50996077a16b
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-10-25 10:17:26 +0530

    use a global state

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -28,16 +28,17 @@ const addProp = (parent, prop, value) => {
   })
 }
 
-// example 
-const data = [['rocks', 1, 2, 2], ['pebbles', 5, 3, 15], ['gems', 3, 9, 27], ['total', '', '', 44]].map(row => row.reduce((a, e) => a.concat(new Value(e)), []))
+// example
+const state = {}
+state.data = [['rocks', 1, 2, 2], ['pebbles', 5, 3, 15], ['gems', 3, 9, 27], ['total', '', '', 44]].map(row => row.reduce((a, e) => a.concat(new Value(e)), []))
+
+console.log(state)
 
 function getRowHtml(row) {
   return '<tr>' + row.map(e => e.value).reduce((sum, e) => sum + '<td> <input value=' + e + '></input></td>', '') + '</tr>'
 }
 
-console.log(getRowHtml(data[0]))
-
 document.getElementById('table').innerHTML = '<tr> <th> Name </th> <th> Rate </th><th> Quantity </th><th> Amount </th></tr>' +
-      data.map(getRowHtml).reduce((sum, e) => sum + '<tr>' + e + '</tr>')
+      state.data.map(getRowHtml).reduce((sum, e) => sum + '<tr>' + e + '</tr>')
 
 // set up subscribers
